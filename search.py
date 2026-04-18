05cf3dbaa8598a9e6fbd9ab5ce6cbc63b1fa97c8
Varshini Vijay <varshinivijay05@gmail.com>
2026-04-18 06:27:38 -0700

    added categories, crawler

--- search.py
+++ search.py
@@ -4,10 +4,8 @@ from dotenv import load_dotenv
 
 # Load variables from .env
 load_dotenv() 
-
 # Retrieve the key
 API_KEY = os.getenv("CUSTOM_SEARCH_API")
-
 
 def search(query):
     url = "https://www.googleapis.com/customsearch/v1"
